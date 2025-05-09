ac7de552241d68a33d99386a65e241d39313377b
lucasdevfull <lucassilva270805@gmail.com>
2025-05-09 13:37:06 -0300

    Adicionando um serviço para autenticação

---
+++ src/services/auth_service.py
@@ -0,0 +1,16 @@
+from src.domain.entities.user_entity import UserEntity
+from src.repositories.user_repository import UserRepository
+from bcrypt import checkpw
+class AuthService:
+    def __init__(self) -> None:
+        self.user_repository = UserRepository()
+
+    def login(self, username: str, password: str) -> dict | None:
+        user = self.user_repository.get_user(username)
+        if not user:
+            raise ValueError('User not found')
+        
+        if not checkpw(password.encode('utf-8'), user.get('password').encode('utf-8')):
+            return ValueError('Password is incorrect')
+        
+        return user
